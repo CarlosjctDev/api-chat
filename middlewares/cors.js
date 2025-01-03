97dd0806d9250b74128f85acdf3f0a8d24e0eae5
Carlos Julio Castaño Torres <118862658+CarlosjctDev@users.noreply.github.com>
2025-01-02 22:37:25 -0500

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:8080',
   'http://localhost:8081',
   'https://9jc6n1f0-8081.use.devtunnels.ms',
-  'https://api-chat-prueba.onrender.com/'
+  'https://api-chat-prueba.onrender.com'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
